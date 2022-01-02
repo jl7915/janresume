868d46b5da1372d0ca02f82bf09ce102af266db4
Janice Lee <79432194+jl7915@users.noreply.github.com>
2022-01-02 18:44:46 -0500

    Add files via upload

---
+++ resumedata.r
@@ -0,0 +1,36 @@
+education <- tribble(
+  ~ degree, ~ year, ~ institution, ~ where, ~detail,
+  "Bachelor of Arts, Major in Economics", "May 2020", "New York University, College of Arts & Science ", "New York, NY", N/A, "Minor in Business Studies", "May 2020", "Stern School of Business", "New York, NY","Relevant coursework: Statistics, Econometrics, Macroeconomic Analysis, Microeconomic Analysis, Marketing" 
+)
+
+skills <- tribble(
+  ~area, ~skills,
+  N/A, "SQL server, MySQL, PostgreSQL", N/A, "Python (Numpy, Pandas, Matplotlib, Seaborn)", N/A, "R, RStudio, R Markdown", N/A, "SQL server, MySQL, PostgreSQL", N/A, "Microsoft Excel (Pivot tables, VLOOKUP, Conditional Formatting)",
+"Data Visualization tools", "Power BI and Tableau"
+)
+
+certifications <- tribble(
+  ~year, ~desc,
+  "December 2021", "Google Data Analytics Professional Certificate"
+)
+
+experience <- tribble(
+  ~year, ~desc, ~where, ~details,
+  "June 2019 - August 2019", "Financial Sales Intern, Equitable advisors, LLC (formerly AXA Advisors)", "New York, NY"
+,"* Increased efficiency by implementing a new copy and paste leads sheet in excel that enabled team members to filter out unnecessary data when searching for prospective clients in half the original time with utmost accuracy. 
+* Identified over 5,000 new prospects in the New York metropolitan area and collaborated with a mentor to successfully acquire 30 clients in the span of 10 weeks.
+* Extracted information off of platforms such as LinkedIn, Sales Navigator, and Salesforce to create spreadsheets consisting of ideal potential clients.
+* Managed and updated the constantly changing company database for a team of 15 CPA/CFAs."
+)
+
+activities <- tribble(
+  ~year, ~desc, ~where, ~detaills,
+  "August 2016 - December 2020", "Public Relations Director, Give Orange", "New York, NY", 
+"* Managed social media accounts such as Instagram and Facebook, using insights to present the positive effect of certain posts over others through measuring accounts reached and accounts engaged.
+* Promoted on campus fundraisers through social media and word-of-mouth marketing.
+* Recruited new members and communicated with non-members regarding details for upcoming events and donation opportunities.",
+  "August 2016 - August 2018", "Teacher, PCNJ Summer School", " Palisades Park, NJ", "* Facilitated 8th grade mathematics curriculum by developing a 3 month long educational program that couples with New Jersey student learning standards.
+* Worked with students individually to help overcome specific learning challenges by recognizing weaknesses.
+* Researched past standardized tests required by the state to find patterns and recurring topics for students to familiarize and prepare."
+)
+
